0c321e40bb9b41af9350aa8d6396a91b8a0026d3
Christian de Botton <cdebotton@gmail.com>
2015-07-21 20:50:11 -0400

    WebSocket TDD working.

--- tests/WSServerTests.js
+++ tests/WSServerTests.js
@@ -1,15 +1,36 @@
-import http from "http";
+import {Server as http} from "http";
 import chai from "chai";
 import sinonChai from "sinon-chai";
-import ws from "../app/utils/ws-server";
-import wsClient from "ws";
+import io from "socket.io";
+import ioc from "socket.io-client";
 
 chai.should();
 chai.use(sinonChai);
 
-describe("WSServer", () => {
-  it("should accept connections", () => {
-    const server = ws({ port: 9000 });
+const client = (srv, nsp, opts) => {
+  if (typeof nsp === "object") {
+    opts = nsp;
+    nsp = null;
+  }
+  let addr = srv.address();
+  if (!addr) addr = srv.listen().address();
 
+  const url = `ws://localhost:${addr.port + (nsp || "")}`;
+
+  return ioc(url, opts);
+};
+
+describe("WSServer", () => {
+  it("should accept connections", (done) => {
+    const srv = http();
+    const sio = io(srv);
+
+    sio.on("connection", (socket) => {
+      done();
+    });
+
+    srv.listen(() => {
+      const socket = client(srv);
+    });
   });
 });

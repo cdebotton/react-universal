de8836be44f42af1e111da460ba7e55d7d8b7e75
Christian de Botton <cdebotton@gmail.com>
2015-10-07 07:37:27 -0400

    Add graphQLHTTP
    Include the express-graphql package.

--- data/index.js
+++ data/index.js
@@ -2,7 +2,14 @@
 
 import express from 'express';
 import graphQLHTTP from 'express-graphql';
+import { Schema } from './schema';
 
 const app = express();
+
+app.use(graphQLHTTP(request => ({
+  schema: Schema,
+  pretty: true,
+  rootValue: { sid: request.headers.authorization },
+})));
 
 export default app;

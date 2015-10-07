0f5ed2a960d7692bb4d777e19aa4d9bbed269533
Christian de Botton <cdebotton@gmail.com>
2015-10-07 00:11:51 -0400

    Add Linting
    Move preLoaders linting to production build rather than in the base config.
    Also, lint production.config.js file.

--- build/webpack/production.config.js
+++ build/webpack/production.config.js
@@ -1,10 +1,7 @@
 import webpack from 'webpack';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import base from './base';
-import {
-  webpackHmrPath,
-  paths,
-} from '../../config';
+import { paths } from '../../config';
 
 export default {
   ...base,
@@ -12,6 +9,13 @@ export default {
   target: 'web',
   module: {
     ...base.module,
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint',
+      },
+    ],
     loaders: base.module.loaders.map(loaderConfig => {
       if (/css/.test(loaderConfig.test)) {
         const [first, ...rest] = loaderConfig.loaders;

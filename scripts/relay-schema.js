1edb064b0e08b4d983ecb1a885ebf8a72c7d1c74
Christian de Botton <cdebotton@gmail.com>
2015-10-07 07:47:34 -0400

    Add Relay Schema script
    Write initial script to build relay schema.

---
+++ scripts/relay-schema.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node_modules/.bin/babel-node
+
+/* @flow */
+
+import fs from 'fs';
+import path from 'path';
+import { graphql } from 'graphql';
+import { introspectionQuery } from 'graphql/utils';
+import { Schema } from '../data/schema';
+
+(async () => {
+  const result = await graphql(Schema, introspectionQuery);
+
+  if (result.errors) {
+    console.error(`Error: ${JSON.stringify(result.errors, null, 2)}`);
+  } else {
+    const target = path.join(__dirname, '..', 'dist', 'server', 'schema.json');
+    fs.writeFileSync(target, JSON.stringify(result, null, 2));
+  }
+})();

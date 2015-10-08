d9d7767ee6875b8650f68afe631e246245c53ae5
Christian de Botton <cdebotton@gmail.com>
2015-10-08 10:58:26 -0400

    Add production server.
    Use `npm start` and `npm stop` to start and stop the server.
    Starting builds the production config and relay config, and stopping will
    clear out the `./dist/*` folder.

--- index.js
+++ index.js
@@ -4,29 +4,35 @@ var createMonitor = require('spawn-monitor').createMonitor;
 var config = require('./config');
 var paths = config.paths;
 var __DEV__ = config.globals.__DEV__;
+var __PROD__ = config.globals.__PROD__;
 
-createMonitor({
-  script: paths.bin('client'),
-  key: 'client',
-  rebootOnChange: [paths.app()],
-});
-
-createMonitor({
-  script: './bin/graphql',
-  key: 'graphql',
-});
+if (__PROD__) {
+  require(paths.bin('client'));
+  require(paths.bin('graphql'));
+}
 
 if (__DEV__) {
   createMonitor({
-    script: './bin/dev-server',
+    script: paths.bin('client'),
+    key: 'client',
+  });
+
+  createMonitor({
+    script: paths.bin('graphql'),
+    key: 'graphql',
+  });
+
+
+  createMonitor({
+    script: paths.bin('dev-server'),
     key: 'dev-server',
   });
-}
 
-createMonitor({
-  script: './bin/relay',
-  key: 'relay',
-});
+  createMonitor({
+    script: paths.bin('relay'),
+    key: 'relay',
+  });
+}
 
 // createMonitor({
 //   script: './bin/test',

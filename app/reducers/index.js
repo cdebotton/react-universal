fa326e33ab9f49e9f2119e769e4350014550f864
Christian de Botton <cdebotton@gmail.com>
2015-10-24 10:08:01 -0400

    Include formReducer
    redux-form had been installed, but the reducer had yet to be included
    in the store.

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,7 +1,12 @@
 /* @flow */
 
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import app from 'reducers/app';
 import session from 'reducers/session';
 
-export default combineReducers({ app, session });
+export default combineReducers({
+  app,
+  session,
+  form: formReducer,
+});
